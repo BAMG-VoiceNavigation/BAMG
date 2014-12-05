950f856e6b53997850d49b9d45e3a8a6f4206473
Alexandru Tifrea <atifrea@mozilla.com>
2014-12-05 20:19:01 +0200

    Enable the port

--- voiceRec.js
+++ voiceRec.js
@@ -60,27 +60,27 @@ window.onload = function (e) {
                     case "previous":
                     case "bac":
                     case "back":
-//                         port.postMessage({
-//                             command: "Back"
-//                         });
+                        port.postMessage({
+                            command: "Back"
+                        });
                         is_news_feed = 0;
                         $(document).find('.snowliftPager').get(0).click();
                         break;
 
                     case "cancel":
-//                         port.postMessage({
-//                             command: "Cancel"
-//                         });
+                        port.postMessage({
+                            command: "Cancel"
+                        });
                         is_searching = -1;
                         is_commenting = 0;
                         break;
 
                     case "comment":
-//                         port.postMessage({
-//                             command: "Comment"
-//                         });
+                        port.postMessage({
+                            command: "Comment"
+                        });
                         if (document.location.href == "https://www.facebook.com/") {
-                            var post = document.getElementsByClassName("userContentWrapper")[scroll_index-2];
+                            var post = document.getElementsByClassName("userContentWrapper")[scroll_index-1];
                             $(post).find('.uiLinkButton').get(0).click();
                             /////////
                             comment_box_input = $(post).find('.hiddenInput').get(0);
@@ -94,15 +94,16 @@ window.onload = function (e) {
                     case "download":
                     case "braun":
                     case "down":
-//                         port.postMessage({
-//                             command: "Down"
-//                         });
+                    case "dawn":
+                        port.postMessage({
+                            command: "Down"
+                        });
                         if (document.location.href == "https://www.facebook.com/") {
                             ana_banana = true;
                         }
                         if (document.location.href == "https://www.facebook.com/") {
                             var post = document.getElementsByClassName("userContentWrapper")[scroll_index];
-                            scroll_index+=2;
+                            scroll_index+=1;
                             console.log("Down");
                             console.log(post);
                             $(window).scrollTo($(post), {
@@ -116,9 +117,9 @@ window.onload = function (e) {
 
                     case "pause":
                     case "exit":
-//                         port.postMessage({
-//                             command: "Exit, recording will now stop"
-//                         });
+                        port.postMessage({
+                            command: "Exit, recording will now stop"
+                        });
                         exit_status = true;
                         break;
 
@@ -128,9 +129,9 @@ window.onload = function (e) {
                         break;
 
                     case "friendrequest":
-//                         port.postMessage({
-//                             command: "Friend requests"
-//                         });
+                        port.postMessage({
+                            command: "Friend requests"
+                        });
                         is_news_feed = 0;
                         location.href = "https://www.facebook.com/friends/requests";
                         break;
@@ -138,9 +139,9 @@ window.onload = function (e) {
                     case "ga":
                     case "gau":
                     case "go":
-//                         port.postMessage({
-//                             command: "Go"
-//                         });
+                        port.postMessage({
+                            command: "Go"
+                        });
                         is_news_feed = 0;
                         if (is_searching == 0) {
                             //console.log($(document).find("div.instant_search_title a")[0]);
@@ -151,11 +152,11 @@ window.onload = function (e) {
                         break;
 
                     case "like":
-//                         port.postMessage({
-//                             command: "Like"
-//                         });
+                        port.postMessage({
+                            command: "Like"
+                        });
                         if (is_news_feed == 1) {
-                            var post = document.getElementsByClassName("userContentWrapper")[scroll_index-2];
+                            var post = document.getElementsByClassName("userContentWrapper")[scroll_index-1];
                             console.log(post);
                             $(post).find('.UFILikeLink').get(0).click();
                         } else {
@@ -166,42 +167,44 @@ window.onload = function (e) {
                     case "message":
                     case "messenger":
                     case "messages":
-//                         port.postMessage({
-//                             command: "Messages"
-//                         });
+                        port.postMessage({
+                            command: "Messages"
+                        });
                         is_news_feed = 0;
                         user = location.href.split(".com/")[1].split('?')[0];
                         location.href = "https://www.facebook.com/messages/" + user;
                         break;
 
                     case "next":
-//                         port.postMessage({
-//                             command: "Next"
-//                         });
+                        port.postMessage({
+                            command: "Next"
+                        });
                         is_news_feed = 0;
                         $(document).find('.snowliftPager').get(1).click();
                         break;
 
                     case "notification":
                     case "notifications":
-//                         port.postMessage({
-//                             command: "Notifications"
-//                         });
+                        port.postMessage({
+                            command: "Notifications"
+                        });
                         location.href = "https://www.facebook.com/notifications";
                         break;
 
                     case "profile":
-//                         port.postMessage({
-//                             command: "Profile"
-//                         });
+                        port.postMessage({
+                            command: "Profile"
+                        });
                         is_news_feed = 0;
-                        location.href = $(document).find('div.clearfix._5pcr.userContentWrapper').eq(scroll_index - 1).find('div a:first').attr('href');
+//                         location.href = $(document).find('div.clearfix._5pcr.userContentWrapper').eq(scroll_index - 1).find('div a:first').attr('href');
+                        var post = document.getElementsByClassName("userContentWrapper")[scroll_index-1];
+                        $(post).find('div a:first').attr('href');
                         break;
 
                     case "photos":
-//                         port.postMessage({
-//                             command: "Photos"
-//                         });
+                        port.postMessage({
+                            command: "Photos"
+                        });
                         is_news_feed = 0;
                         location.href = location.href.split('?')[0] + '/photos';
                         break;
@@ -211,12 +214,12 @@ window.onload = function (e) {
                     case "upc":
                     case "up":
                         if (document.location.href == "https://www.facebook.com/") {
-//                             port.postMessage({
-//                                 command: "Up"
-//                             });
-                            scroll_index-=2;
+                            port.postMessage({
+                                command: "Up"
+                            });
+                            scroll_index-=1;
                             if (scroll_index >= 0) {
-                                var post = document.getElementsByClassName("userContentWrapper")[scroll_index-2];
+                                var post = document.getElementsByClassName("userContentWrapper")[scroll_index-1];
 //                                 var post = $(".clearfix._5pcr.userContentWrapper").get(scroll_index - 1);
                                 $(window).scrollTo($(post), {
                                     offset: -50,
@@ -233,29 +236,29 @@ window.onload = function (e) {
 
                     case "resume":
                     case "start":
-//                         port.postMessage({
-//                             command: "Starting to record"
-//                         });
+                        port.postMessage({
+                            command: "Starting to record"
+                        });
                         exit_status = false;
                         break;
 
                     case "sarci":
                     case "search":
-//                         port.postMessage({
-//                             command: "Search"
-//                         });
+                        port.postMessage({
+                            command: "Search"
+                        });
                         is_news_feed = 0;
                         is_searching = 1;
                         break;
 
                     case "cher":
                     case "share":
-//                         port.postMessage({
-//                             command: "Share"
-//                         });
+                        port.postMessage({
+                            command: "Share"
+                        });
                         if (is_news_feed == 1) {
                             if (is_sharing == -1) {
-                                var post = document.getElementsByClassName("userContentWrapper")[scroll_index-2];
+                                var post = document.getElementsByClassName("userContentWrapper")[scroll_index-1];
                                 $(post).find('.share_action_link').get(0).click();
                                 is_sharing = 1;
                             } else if (is_sharing == 0) {
@@ -267,18 +270,18 @@ window.onload = function (e) {
                     case "loom":
                     case "zoom":
                     case "xoom":
-//                         port.postMessage({
-//                             command: "Zoom"
-//                         });
+                        port.postMessage({
+                            command: "Zoom"
+                        });
                         is_news_feed = 0;
                         console.log($(document).find('a.uiMediaThumb._6i9.uiMediaThumbMedium').get(1));
                         $(document).find('a.uiMediaThumb._6i9.uiMediaThumbMedium').get(0).click();
                         break;
 
                     default:
-//                         port.postMessage({
-//                             command: "Sorry, I didn't get that."
-//                         })
+                        port.postMessage({
+                            command: "Sorry, I didn't get that."
+                        })
                         console.log("default");
                         // begin if is_searching
                         if (is_searching == 1) {
@@ -293,10 +296,10 @@ window.onload = function (e) {
                         // begin if is_commenting
                         if (location.href.split('messages').length == 2) {
                             // we're in messages
-                            $(document).find("textarea").get(0).value += raw_command + " ";
+                            $(document).find("textarea").get(0).value += raw_command + "\n";
                             // TODO
                             // trebuie imbunatatita partea asta
-                            $(".uiButtonConfirm:nth-child(2)")[1].control.click();
+//                             $(".uiButtonConfirm:nth-child(2)")[1].control.click();
                         }
                         if (is_commenting == 1) {
                             comment_box.value = command;
